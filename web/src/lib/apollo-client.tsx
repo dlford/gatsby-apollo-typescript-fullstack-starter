0494ac92a19a1cb8bc107082981ab0d38d3cf92b
dlford <dan-github2@dlford.co>
2020-05-13 23:20:34 -0700

    Add Apollo httpLink to client, only use uploadLink for uploads

--- web/src/lib/apollo-client.tsx
+++ web/src/lib/apollo-client.tsx
@@ -3,26 +3,30 @@ import ApolloClient from 'apollo-client'
 import { split } from 'apollo-link'
 import { setContext } from 'apollo-link-context'
 import { createUploadLink } from 'apollo-upload-client'
+import { createHttpLink } from 'apollo-link-http'
 import { getMainDefinition } from 'apollo-utilities'
 import { SubscriptionClient } from 'subscriptions-transport-ws'
 import { WebSocketLink } from 'apollo-link-ws'
-import fetch from 'isomorphic-unfetch'
+
+interface LinkDefinition {
+  kind: string
+  operation?: string
+}
 
 const useApollo = (token: string | void) => {
-  interface LinkDefinition {
-    kind: string
-    operation?: string
-  }
-
   const apiUrl =
     process.env.GATSBY_API_URL || 'http://localhost:3000/graphql'
   const wsUrl =
     process.env.GATSBY_WS_URL || apiUrl.replace(/^http/, 'ws')
 
+  const httpLink = createHttpLink({
+    uri: apiUrl,
+    credentials: 'include', // TODO
+  })
+
   const uploadLink = createUploadLink({
     uri: apiUrl,
     credentials: 'include', // TODO
-    fetch,
   })
 
   const subscriptionClient = new SubscriptionClient(wsUrl, {
@@ -35,7 +39,7 @@ const useApollo = (token: string | void) => {
 
   const wsLink = new WebSocketLink(subscriptionClient)
 
-  const link = split(
+  const requestLink = split(
     ({ query }) => {
       const { kind, operation }: LinkDefinition = getMainDefinition(
         query,
@@ -45,7 +49,19 @@ const useApollo = (token: string | void) => {
       )
     },
     wsLink,
+    httpLink,
+  )
+
+  const terminalLink = split(
+    ({ variables }): boolean => {
+      return Object.values(variables).some(
+        (value) =>
+          (typeof File !== 'undefined' && value instanceof File) ||
+          (typeof Blob !== 'undefined' && value instanceof Blob),
+      )
+    },
     uploadLink,
+    requestLink,
   )
 
   const authLink = setContext((request, { headers }) => {
@@ -61,7 +77,7 @@ const useApollo = (token: string | void) => {
 
   return {
     client: new ApolloClient({
-      link: authLink.concat(link),
+      link: authLink.concat(terminalLink),
       cache: new InMemoryCache(),
     }),
     subscriptionClient,
